5df93c1c6bfc9782ea78da50c3a2640f5e9e8768
Chris Geekie <chris.geekie@gmail.com>
2021-12-01 19:12:08 +0100

    More iterator magic

--- aoc2021/day_1.py
+++ aoc2021/day_1.py
@@ -16,13 +16,8 @@ def count_increases(data: List[int]) -> int:
 
 
 def count_window_increases(data: List[int]) -> int:
-    total: int = 0
     window_data = list(map(sum, zip(data, data[1:], data[2:])))
-    for measures in zip(window_data, window_data[1:]):
-        if measures[1] > measures[0]:
-            total = total + 1
-
-    return total
+    return count_increases(window_data)
 
 
 def main() -> None:
