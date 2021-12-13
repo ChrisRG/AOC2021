4f749ccf442113cfe315469b40550a69c7074c8b
Chris Geekie <chris.geekie@gmail.com>
2021-12-13 18:04:47 +0100

    Day 13 Part 2

--- aoc2021/day_13.py
+++ aoc2021/day_13.py
@@ -14,11 +14,22 @@ def load_data(filepath: str) -> tuple[List, List]:
         return ([(int(pair[0]), int(pair[1])) for pair in dots], folds)
 
 
-def fold(coords: List[List[int]], fold: tuple[str, int]):
+def print_origami(coords: List[tuple[int, int]]):
+    x_size = sorted(coords, key=lambda tup: tup[0])[-1][0]
+    y_size = sorted(coords, key=lambda tup: tup[1])[-1][1]
+    grid = [[" "] * (x_size + 1) for _ in range(y_size + 1)]
+    for pair in coords:
+        grid[pair[1]][pair[0]] = "#"
+    for line in grid:
+        print(f"{' '.join(line)}\n")
+
+
+def fold(coords: List, fold: tuple[str, int]) -> List[tuple[int, int]]:
     # Y = 2 * fold - Y/X coord
     axis, amt = fold
     new_pairs = []
     old_pairs = []
+    # Transform coordinates based on fold, a bit sloppy but functional
     for pair in coords:
         if axis == "y":
             if pair[1] > amt:
@@ -28,7 +39,8 @@ def fold(coords: List[List[int]], fold: tuple[str, int]):
             if pair[0] > amt:
                 new_pairs.append((2 * amt - pair[0], pair[1]))
                 old_pairs.append(pair)
-    combos = (set(coords) - set(old_pairs)).union(set(new_pairs))
+    # To merge points, treat coordinates as sets, removing old pairs and joining new ones
+    combos = list((set(coords) - set(old_pairs)).union(set(new_pairs)))
     return combos
 
 
@@ -37,7 +49,10 @@ def test_data():
     (coords, folds) = load_data("inputs/day_13_test.txt")
     coords = fold(coords, folds[0])
     print(f"Test 1: {len(coords)}")
+    for fld in folds[1:]:
+        coords = fold(coords, fld)
     print("Test passed.")
+    print_origami(coords)
 
 
 def main():
@@ -45,6 +60,11 @@ def main():
     (coords, folds) = load_data("inputs/day_13_input.txt")
     coords = fold(coords, folds[0])
     print(f"Part 1: {len(coords)}")
+
+    for fld in folds[1:]:
+        coords = fold(coords, fld)
+    print("Part 2:")
+    print_origami(coords)
 
 
 if __name__ == "__main__":
